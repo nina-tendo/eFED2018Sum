87a7e1f590740815db693728e3bc2837b0f65efe
nina-tendo <40564636+nina-tendo@users.noreply.github.com>
2018-08-30 12:02:30 +0400

    Add files via upload

---
+++ js/html.js
@@ -0,0 +1,18 @@
+var temp = document.getElementsByClassName('graph_container');
+var rainFall = document.getElementsByClassName('rainfall_expectancy');
+var wind = document.getElementsByClassName('wind-and-time');
+var buttonTemp = document.getElementsByClassName('temperature');
+var buttonRain = document.getElementsByClassName('rainfall');
+var buttonWind = document.getElementsByClassName('wind');
+buttonTemp[0].addEventListener('click', temperature);
+buttonRain[0].addEventListener('click', rainfall);
+buttonWind[0].addEventListener('click', windy);
+function temperature (event){
+	
+}
+function rainfall (event){
+
+}
+function windy (event){
+
+}
--- js/html5.js
+++ js/html5.js
@@ -1,6 +1,51 @@
-var bkg = document.getElementsByClassName('button');
-function setBackground(event){
-    bkg.classList.add('active');
-    return;
+var bkg = document.getElementsByClassName('buttons');
+bkg[0].addEventListener('click', function setBackground(event){ 
+	if (event.target.id === 'button'){
+	for (var i = 0; i < bkg.length; i++){
+		bkg[i].childNodes.classList.add('active');
+		break;
+		}
+	}
+});
+var leftArrow = document.getElementById('left_arrow');
+var rightArrow = document.getElementById('right_arrow');
+var blockListener = document.getElementsByClassName('js_media');
+var todayBlocks = document.getElementsByClassName('today_block');
+blockListener[0].addEventListener('click',function(event) {
+	if (event.target.id === 'left_arrow') {
+		showPrev()
+	} else if (event.target.id === 'right_arrow') {
+		showNext();	
+	}
+});
+
+function showNext() {
+	for (var i = 0; i < todayBlocks.length; i++) {
+		if (todayBlocks[i].classList.contains('activeDay')) {
+			todayBlocks[i].classList.remove('activeDay');
+
+			if (i === todayBlocks.length - 1) {
+				todayBlocks[0].classList.add('activeDay');
+				break;
+			}
+			
+			todayBlocks[i].nextElementSibling.classList.add('activeDay');
+			break;
+		}
+	}
 }
-bkg.addEventListener('click',setBackground,false)
+function showPrev() {
+	for (var i = 0; i < todayBlocks.length; i++) {
+	 	if(todayBlocks[i].classList.contains('activeDay')) {
+			todayBlocks[i].classList.remove('activeDay');
+
+			if (i === todayBlocks.length - 5) {
+				todayBlocks[4].classList.add('activeDay');
+				break;
+			}
+			
+			todayBlocks[i].previousElementSibling.classList.add('activeDay');
+			break;
+		}
+	}
+}
