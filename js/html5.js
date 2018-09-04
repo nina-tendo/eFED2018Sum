c0d1496ba8166389c1865100b074d4ba4b8d5458
nina-tendo <40564636+nina-tendo@users.noreply.github.com>
2018-09-04 21:17:34 +0400

    Add files via upload

--- js/html5.js
+++ js/html5.js
@@ -1,12 +1,39 @@
 var buttons = document.getElementsByClassName('buttons');
+var button = document.querySelectorAll('button');
 buttons[0].addEventListener('click', function (event){ 
 		buttons[0].classList.add('active');
 			var oldActiveElement = document.querySelector('.active');
 			oldActiveElement.classList.remove('active');
-			
 			var newActiveElement = event.target;
-			newActiveElement.classList.add('active');		
-});
+				newActiveElement.classList.add('active');
+			if (event.target.classList.contains('active')) {      
+				showDay(event.target.dataset.index);      
+				for (let i = 0; i < buttons.length; i++) {        
+					if (buttons[i].classList.contains('activeDay')) {          
+						buttons[i].classList.remove('activeDay');
+					  buttons[event.target.dataset.index].classList.add('activeDay');          
+					  break;        
+						}	  
+					}    
+				}  
+			});
+			// var newActiveElement = event.target;
+			// 	newActiveElement.classList.toggle('active');
+			// 	for (var i = 0; i < todayBlocks.length; i++) {
+			// 		if (todayBlocks[i].classList.contains('activeDay')) {
+			// 			todayBlocks[i].classList.remove('activeDay');
+			
+			// 			if (i === button[0].dataset.index) {
+			// 				todayBlocks[i].classList.add('activeDay');
+			// 				break;
+			// 			}
+						
+						// todayBlocks[i].classList.add('activeDay');
+						// break;
+// 					}
+// 				}
+// });
+
 var leftArrow = document.getElementById('left_arrow');
 var rightArrow = document.getElementById('right_arrow');
 var blockListener = document.getElementsByClassName('js_media');
@@ -18,7 +45,18 @@ blockListener[0].addEventListener('click',function(event) {
 		showNext();	
 	}
 });
+function showDay() {
+	for (var i = 0; i < todayBlocks.length; i++) {
+		if (todayBlocks[i].classList.contains('activeDay')) {
+			todayBlocks[i].classList.remove('activeDay');
 
+			if (event.target.id === 'button') {
+				todayBlocks[i].classList.add('activeDay');
+				break;
+			}
+		}
+	}
+}
 function showNext() {
 	for (var i = 0; i < todayBlocks.length; i++) {
 		if (todayBlocks[i].classList.contains('activeDay')) {
