62f656609244694b9e4cb2e77d394921aae95b33
nina-tendo <40564636+nina-tendo@users.noreply.github.com>
2018-09-05 21:12:17 +0400

    Add files via upload

--- xml.js
+++ xml.js
@@ -1,10 +1,10 @@
 var xhr = new XMLHttpRequest(); 
 
-  xhr.open('GET', 'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22', true); 
+  xhr.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?appid=e33262cd6a432b1c3dc5181a736dbc41&q=tokyo', true); 
  
   xhr.send(); 
 
-  xhr.onreadystatechange = function() { 
+  xhr.onload = function () { 
 
     if (this.readyState != 4) return; 
 
@@ -12,7 +12,35 @@ var xhr = new XMLHttpRequest();
       alert('ошибка: ' + (this.status ? this.statusText : 'запрос не удался')); 
       return; 
     }
-
-    alert(xhr.responseText);
+    parseData(xhr.responseText);
 
   }; 
+function parseData (response){
+ 
+  newArray = JSON.parse(response);
+  newObj = newArray;
+ 
+
+  render(renderData);
+}
+// var renderData = {
+// 	city:
+// 	average-max:
+// 	average-min:
+// 	all-max:
+// 	all-min:
+// };
+function render(obj){
+ var current = document.getElementsByTagName('td');
+}
+// отображение по мокам
+var date = new Date(2016, 0);
+var city = 'London';
+load(city,date);
+function load (city,date) {
+	var year = date.getFullYear();
+	var data = ;
+}
+var td = document.querySelectorAll('table tr:nth-child(2)')[0].children;
+// td.slice(1);
+console.log([].slice.call(td, 1));
