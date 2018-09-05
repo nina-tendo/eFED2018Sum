2d8ea76afaea4379dcc703c2acda2c43420fc574
nina-tendo <40564636+nina-tendo@users.noreply.github.com>
2018-09-05 16:48:33 +0400

    Add files via upload

--- js/html.js
+++ js/html.js
@@ -4,36 +4,55 @@ var wind = document.getElementsByClassName('wind-and-time');
 var buttonTemp = document.getElementsByClassName('temperature');
 var buttonRain = document.getElementsByClassName('rainfall');
 var buttonWind = document.getElementsByClassName('wind');
-buttonTemp[0].addEventListener('click', temperature);
-buttonRain[0].addEventListener('click', rainFall);
-buttonWind[0].addEventListener('click', windy);
-function temperature (event){
-    if (temp[0].classList.contains('active')) {
-        temp[0].classList.remove('active');
-    }
-	if (event.target.id === 'temperature'){
-            rainfall[0].classList.remove('acive');
-            wind[0].classList.remove('acive');
-            temp[0].classList.add('active');
+var buttonsBlock = document.querySelector('#buttons');
+var diag = document.getElementsByClassName('diag');
+// buttonTemp[0].addEventListener('click', temperature);
+// buttonRain[0].addEventListener('click', rain);
+// buttonWind[0].addEventListener('click', windy);
+// function temperature (event){
+//         buttonTemp[0].classList.add('activeButton')
+//     if (buttonTemp[0].classList.contains('activeButton')) {
+//          temp[0].classList.remove('active');
+//     }
+// 	if (event.target.dataset.index == 0){
+//             // rainfall[0].classList.remove('active');
+//             // wind[0].classList.remove('active');
+//             temp[0].classList.add('active');
+//     }
+// }
+// function rain (event){
+//         buttonRain[0].classList.add('activeButton')
+//     if (buttonRain[0].classList.contains('activeButton')) {
+//          rainfall[0].classList.remove('active');
+//     }
+//     if (event.target.dataset.index == 1){
+//             // temp[0].classList.remove('active');
+//             // wind[0].classList.remove('active');
+//            rainFall[0].classList.add('active');
+//     }
+// }
+// function windy (event){
+//         buttonWind[0].classList.add('activeButton')
+//     if (buttonWind[0].classList.contains('activeButton')) {
+//          wind[0].classList.remove('active');
+//     }
+//     if (event.target.dataset.index == 2){
+//             // temp[0].classList.remove('active');
+//             // rainfall[0].classList.remove('active');
+//             wind[0].classList.add('active');
+//     }
+// }
+buttonsBlock.onclick = function(event) {
+    if (event.target.classList.contains('button')) {
+        showDiag(event.target.dataset.index);	
     }
 }
-function rainfall (event){
-    if (rainfall[0].classList.contains('active')) {
-        rainfall[0].classList.remove('active');
-    }
-    if (event.target.id === 'rainfall'){
-            wind[0].classList.remove('acive');
-            temp[0].classList.remove('active');
-            rainFall[0].classList.add('active');
-    }
-}
-function windy (event){
-    if (wind[0].classList.contains('active')) {
-        wind[0].classList.remove('active');
-    }
-    if (event.target.id === 'wind'){
-            temp[0].classList.remove('active');
-            rainfall[0].classList.remove('acive');
-            wind[0].classList.add('active');
-    }
-}
+function showDiag(index) {	
+    for (var i = 0; i < diag.length; i++) {		
+        if (diag[i].classList.contains('active')) {			
+            diag[i].classList.remove('active');						
+            diag[index].classList.add('active');			
+            break;		
+            }
+        }
+    }	
--- js/html5.js
+++ js/html5.js
@@ -1,62 +1,45 @@
-var buttons = document.getElementsByClassName('buttons');
-var button = document.querySelectorAll('button');
-buttons[0].addEventListener('click', function (event){ 
-		buttons[0].classList.add('active');
-			var oldActiveElement = document.querySelector('.active');
-			oldActiveElement.classList.remove('active');
-			var newActiveElement = event.target;
-				newActiveElement.classList.add('active');
-			if (event.target.classList.contains('active')) {      
-				showDay(event.target.dataset.index);      
-				for (let i = 0; i < buttons.length; i++) {        
-					if (buttons[i].classList.contains('activeDay')) {          
-						buttons[i].classList.remove('activeDay');
-					  buttons[event.target.dataset.index].classList.add('activeDay');          
-					  break;        
-						}	  
-					}    
-				}  
-			});
-			// var newActiveElement = event.target;
-			// 	newActiveElement.classList.toggle('active');
-			// 	for (var i = 0; i < todayBlocks.length; i++) {
-			// 		if (todayBlocks[i].classList.contains('activeDay')) {
-			// 			todayBlocks[i].classList.remove('activeDay');
-			
-			// 			if (i === button[0].dataset.index) {
-			// 				todayBlocks[i].classList.add('activeDay');
-			// 				break;
-			// 			}
-						
-						// todayBlocks[i].classList.add('activeDay');
-						// break;
-// 					}
-// 				}
-// });
 
-var leftArrow = document.getElementById('left_arrow');
-var rightArrow = document.getElementById('right_arrow');
-var blockListener = document.getElementsByClassName('js_media');
-var todayBlocks = document.getElementsByClassName('today_block');
-blockListener[0].addEventListener('click',function(event) {
-	if (event.target.id === 'left_arrow') {
-		showPrev()
-	} else if (event.target.id === 'right_arrow') {
-		showNext();	
-	}
-});
-function showDay() {
-	for (var i = 0; i < todayBlocks.length; i++) {
-		if (todayBlocks[i].classList.contains('activeDay')) {
-			todayBlocks[i].classList.remove('activeDay');
-
-			if (event.target.id === 'button') {
-				todayBlocks[i].classList.add('activeDay');
-				break;
+				var buttons = document.getElementsByClassName('buttons');
+			var buttonsBlock = document.querySelector('#buttons');
+			var button = document.querySelectorAll('.button');
+			buttonsBlock.onclick = function(event) {	
+				if (event.target.classList.contains('button')) {		
+					showDay(event.target.dataset.index);	
+				}
 			}
+				buttons[0].addEventListener('click', function (event){ 
+				buttons[0].classList.add('active');
+					 if (this.classList.contains('active')){
+						this.classList.remove('active');
+						event.target.classList.toggle('active');
+				}
+			});
+		buttonsBlock.onclick = function(event) {
+		if (event.target.classList.contains('button')) {
+			showDay(event.target.dataset.index);	
 		}
 	}
-}
+					var leftArrow = document.getElementById('left_arrow');
+					var rightArrow = document.getElementById('right_arrow');
+					var blockListener = document.getElementsByClassName('js_media');
+					var todayBlocks = document.getElementsByClassName('today_block');
+					blockListener[0].addEventListener('click',function(event) {	
+						if (event.target.id === 'left_arrow') {		
+							showPrev()	
+						} else if (event.target.id === 'right_arrow') {		
+								showNext();		
+							}
+						});
+function showDay(index) {	
+for (var i = 0; i < todayBlocks.length; i++) {		
+	if (todayBlocks[i].classList.contains('activeDay')) {			
+		todayBlocks[i].classList.remove('activeDay');			
+		// TODO: change nav button style			
+		todayBlocks[index].classList.add('activeDay');			
+		break;		
+		}
+	}
+}	
 function showNext() {
 	for (var i = 0; i < todayBlocks.length; i++) {
 		if (todayBlocks[i].classList.contains('activeDay')) {
@@ -64,11 +47,13 @@ function showNext() {
 
 			if (i === todayBlocks.length - 1) {
 				todayBlocks[0].classList.add('activeDay');
+				button[0].classList.add('active');	
 				break;
 			}
 			
-			todayBlocks[i].nextElementSibling.classList.add('activeDay');
-			break;
+			todayBlocks[i].nextElementSibling.classList.add('activeDay');			
+				button[i].nextElementSibling.classList.add('active');	
+				break;		
 		}
 	}
 }
@@ -79,11 +64,12 @@ function showPrev() {
 
 			if (i === todayBlocks.length - 5) {
 				todayBlocks[4].classList.add('activeDay');
+				button[4].classList.add('active');	
 				break;
 			}
 			
 			todayBlocks[i].previousElementSibling.classList.add('activeDay');
-			break;
+			button[i].previousElementSibling.classList.add('active');	break;
 		}
 	}
 }
