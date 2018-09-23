121854164d9caf7cbf826a31f5155e773ac87d15
nina-tendo <40564636+nina-tendo@users.noreply.github.com>
2018-09-23 20:06:50 +0400

    Add files via upload

--- xml-additional_page.js
+++ xml-additional_page.js
@@ -1,10 +1,5 @@
-var xhr = new XMLHttpRequest(); 
-
-  xhr.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?appid=e33262cd6a432b1c3dc5181a736dbc41&q=tokyo', true); 
- 
-  xhr.send(); 
-
-  xhr.onload = function () { 
+let request = fetch('api.openweathermap.org/data/2.5/forecast?appid=e33262cd6a432b1c3dc5181a736dbc41&q=tokyo') 
+  .then(function () { 
 
     if (this.readyState != 4) return; 
 
@@ -12,15 +7,12 @@ var xhr = new XMLHttpRequest();
       alert('ошибка: ' + (this.status ? this.statusText : 'запрос не удался')); 
       return; 
     }
-    parseData(xhr.responseText);
+    parseData(response);
 
-  }; 
+  }); 
 function parseData (response){
- 
   newArray = JSON.parse(response);
   newObj = newArray;
- 
-
   render(renderData);
 }
 var renderData = {
@@ -28,7 +20,7 @@ var renderData = {
 	
 };
 function render(obj){
- var current = document.getElementsByTagName('td');
+
 }
 var input = document.querySelector('input');
 input.onkeydown = function (event){
